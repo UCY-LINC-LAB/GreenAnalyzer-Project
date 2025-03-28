eccc42cdb4da0a6ccd9145741e36e8b5d4b2893b
nicoltsiop <158579212+nicoltsiop@users.noreply.github.com>
2025-03-28 10:51:43 +0200

    Add files via upload

--- GreenAnalyzerService/GreenAnalyzer/models.py
+++ GreenAnalyzerService/GreenAnalyzer/models.py
@@ -15,21 +15,61 @@ class RESModel(models.Model):
         ('MATH', 'MATH')
     ]
 
+    METRIC_CHOICES = [
+        ('MAE', 'MAE'),
+        ('MSE', 'MSE'),
+        ('RMSE', 'RMSE'),
+        ('R2', 'R2'),
+        ('RMSLE', 'RMSLE'),
+        ('MAPE', 'MAPE'),
+        ('SMAPE', 'SMAPE')
+    ]
+
+        # identity = 1
+        # id_provided = True
+
+        # date_col = 'date'
+        # pv_col = 'inverter1'
+
+        # latitude = 35.1542
+        # longitude = 33.3964
+        # timezone = 'Europe/Moscow'
+
+        # model_type = 'Naive' #['AI','Naive','Math']
+
+        # panel_tilt = 17
+        # panel_size = 63*1.7*1
+        # n_trials = 100
+
+        # optimize_metric = 'MAPE'
+
     name = models.CharField(max_length=100, unique=True, primary_key=True)
     res_type = models.CharField(max_length=100, choices=STATUS_CHOICES)
+    # days_of_prediction = models.IntegerField(default=2)
     lat = models.FloatField()
     lon = models.FloatField()
+    timezone = models.CharField(max_length=100, default='Europe/Athens')
     model_type = models.CharField(max_length=100, choices=MODEL_CHOICES)
-    parameters = models.JSONField(default=dict)
+    optimize_metric = models.CharField(max_length=100, choices=METRIC_CHOICES, default='MAPE')
+    n_trials = models.IntegerField(default=100)
+
+    date_col = models.CharField(max_length=100, default='date')
+    energy_col = models.CharField(max_length=100, default='energy')
+    panel_tilt = models.FloatField(default=20)
+    panel_size = models.FloatField(default=10)
+
+    parameters = models.JSONField(default=None, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     stored_model = models.FileField(upload_to='energy_pv_models', default=None)
 
+    csv_file = models.FileField(upload_to='dfs', default=None)
+
     def get_model_path(self):
         if self.stored_model:
-            return self.stored_model.path.replace('.pkl', '')
+            return self.stored_model.name.replace('.pkl', '') # self.stored_model.path.replace('.pkl', '')
 
 class EnergyProfile(models.Model):
     name = models.CharField(max_length=100, unique=True, primary_key=True)
--- GreenAnalyzerService/GreenAnalyzer/views.py
+++ GreenAnalyzerService/GreenAnalyzer/views.py
@@ -14,42 +14,73 @@ from .models import RESModel, EnergyProfile
 
 from .serializers import RESSerializer, EnergyProfileSerializer
 from services.modeler import AIModel, NaiveModel, MathModel
+from services.preprocessing_and_training import preprocessing_and_training
+from django.core.files.storage import default_storage
 
 model_mapping = dict(AI=AIModel, NAIVE=NaiveModel, MATH=MathModel)
 
 
 class RESViewSet(viewsets.ModelViewSet):
-    queryset = RESModel.objects.all()
+    queryset = RESModel.objects.none()  # to not print/show all the items of the database
+    #queryset = RESModel.objects.all()
     serializer_class = RESSerializer
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        # preprocessing_and_training(serializer, default_storage.path(instance.csv_file.name))
+        preprocessing_and_training(instance)
+        # parameters_and_model = training(serializer.validated_data['name'], serializer.validated_data['date_col'], serializer.validated_data['energy_col'], 
+        #                                 serializer.validated_data['lat'], serializer.validated_data['lon'], serializer.validated_data['timezone'], 
+        #                                 serializer.validated_data['model_type'], serializer.validated_data['panel_tilt'], serializer.validated_data['panel_size'], 
+        #                                 serializer.validated_data['n_trials'], serializer.validated_data['optimize_metric'], instance.csv_file.name)
+        # serializer.save(parameters = parameters_and_model[0])
+        # serializer.save(stored_model = parameters_and_model[1])
+
 
     @action(detail=True, methods=['get'], name="predict_res")
     def predict(self, request, pk=None):
-        res_model: RESModel = self.get_object()
-        horizon = request.data.get('horizon', 7)
+        # res_model: RESModel = self.get_object()
+        
+        item = RESModel.objects.get(pk=pk)
+        
+        horizon = request.query_params.get('horizon', 7)
 
-        lat = request.data.get("latitude")
-        lon = request.data.get("longitude")
+        lat = request.query_params.get("latitude")
+        lon = request.query_params.get("longitude")
 
         # Retrieve Weather Data
         if lat and lon:
             X_test = WeatherService().get_weather_forecast(float(lat), float(lon), forecast_days=horizon)
         else:
-            X_test = WeatherService().get_weather_forecast(res_model.lat, res_model.lon, forecast_days=horizon)
-
-        ModelClass = model_mapping.get(res_model.model_type)
+            X_test = WeatherService().get_weather_forecast(item.lat, item.lon, forecast_days=horizon)
+ 
+        ModelClass = model_mapping.get(item.model_type)
 
         if ModelClass is None:
             raise Http404
-
-        stored_model_path = res_model.get_model_path()
-
-        model_handler = ModelClass(stored_model_path, params=res_model.parameters)
+        
+        fields = sorted(['apparent_temperature', 'cloud_cover', 
+           'cloud_cover_high', 'cloud_cover_low', 'cloud_cover_mid',
+           'diffuse_radiation', 'diffuse_radiation_instant',
+           'direct_normal_irradiance', 'direct_normal_irradiance_instant',
+           'direct_radiation', 'direct_radiation_instant',
+           'global_tilted_irradiance', 'is_day', 'precipitation', 'pressure_msl',
+           'rain', 'relative_humidity_2m', 'shortwave_radiation',
+           'shortwave_radiation_instant', 'sunshine_duration', 'surface_pressure',
+           'temperature_2m', 'terrestrial_radiation',
+           'terrestrial_radiation_instant', 'wind_direction_100m',
+           'wind_direction_10m', 'wind_gusts_10m', 'wind_speed_100m',
+           'wind_speed_10m', 'azimuth', 'zenith', 'dusk', 'dawn', 'elevation',
+           'golden_hour_start', 'golden_hour_end', 'month', 'hour'])
+        
+        model_handler = ModelClass(item.get_model_path(), params=item.parameters)
         model_handler.retrieve_model()
 
         # Make the prediction
-        output = model_handler.compute_predictions(X_test)
+        output = model_handler.compute_predictions(X_test[fields])
 
         X_test['prediction'] = output
+        X_test['prediction'] = X_test.apply(lambda x: 0 if x['shortwave_radiation']==0 else x['prediction'],axis=1)
         data = X_test[['date', 'prediction']].to_dict('records')
 
         return Response(data, status=status.HTTP_200_OK)
