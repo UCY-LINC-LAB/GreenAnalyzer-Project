756ea33357c1f607340bf563485a576f9c080104
nicoltsiop <158579212+nicoltsiop@users.noreply.github.com>
2025-04-24 13:10:47 +0300

    update 24/04/2025

--- GreenAnalyzerService/entrypoint.sh
+++ GreenAnalyzerService/entrypoint.sh
@@ -5,4 +5,11 @@ python manage.py makemigrations
 python manage.py makemigrations GreenAnalyzer
 python manage.py migrate
 echo "FINISH MIGRATIONS"
+
+# Add cron jobs via django-crontab
+python manage.py crontab remove
+python manage.py crontab add
+# Start cron
+cron
+
 python manage.py runserver 0.0.0.0:8000
